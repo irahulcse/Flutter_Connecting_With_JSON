f6b3a577d44ef8191a5e64f18e8bb258ed86035c
Rahul Chandra <1rahulchandra1@gmail.com>
2020-07-21 22:31:48 +0530

    First Commit using Bash Timestamp(2020-07-21-22:31:45)

---
+++ lib/parsing_json/parsing_json.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart';
+
+class JsonParsing extends StatefulWidget {
+  @override
+  _JsonParsingState createState() => _JsonParsingState();
+}
+
+class _JsonParsingState extends State<JsonParsing> {
+  Future data;
+
+  @override
+  void initState() {
+    super.initState();
+    data = Network("jsonplaceholder.typicode.com/posts").fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Happiness is the key"),
+      ),
+    );
+  }
+
+  Future getData() async {
+    String url;
+    url = "jsonplaceholder.typicode.com/posts";
+    var data;
+    Network network = Network(url);
+    data = network.fetchData();
+    return data;
+  }
+}
+
+//jsonplaceholder.typicode.com/posts
+class Network {
+  final String url;
+  Network(this.url);
+  Future fetchData() async {
+    print("$url");
+    Response response = await get(Uri.encodeFull(url));
+    if (response.statusCode == 200) {
+      print("good");
+      print(response.body);
+      return response.body;
+    } else {
+      print("noy");
+      return response.statusCode;
+    }
+  }
+}
