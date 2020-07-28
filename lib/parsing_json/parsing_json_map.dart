21df2a980b738b51e1790d7d395cc0bc90c7a5d7
Rahul Chandra <1rahulchandra1@gmail.com>
2020-07-28 15:50:04 +0530

    First Commit using Bash Timestamp(2020-07-28-15:50:04)

--- lib/parsing_json/parsing_json_map.dart
+++ lib/parsing_json/parsing_json_map.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:connecting_world_of_flutter/model/post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -7,24 +10,62 @@ class JsonParsingMap extends StatefulWidget {
 }
 
 class _JsonParsingMapState extends State<JsonParsingMap> {
+  Future<PostList> data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Network network = Network("https://jsonplaceholder.typicode.com/posts");
+    data = network.loadPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PODO"),
+      ),
+      body: Center(
+        child:Container(
+          child: FutureBuilder(
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              List<Post>allPosts;
+              if(snapshot.hasData){
+                allPosts=snapshot.data.posts;
+                return Text("${allPosts[0].title}");
+              }
+              else
+              CircularProgressIndicator(); 
+            },
+          ),
+        ),
+      );
+    );
   }
 }
 
 class Network {
   final String url;
   Network(this.url);
-  Future fetchData() async {
-    print("$url");
-    Response response = await get(Uri.encodeFull(url));
+  // Future fetchData() async {
+  //   print("$url");
+  //     Response response = await get(Uri.encodeFull(url));
+  //   if (response.statusCode == 200) {
+  //     // print("good");
+  //     print(response.body);
+  //     return response.body;
+  //   } else {
+  //     return response.statusCode;
+  //   }
+  // }
+  Future<PostList> loadPosts() async {
+    final response = await get(Uri.encodeFull(url));
     if (response.statusCode == 200) {
-      // print("good");
-      print(response.body);
-      return response.body;
+      // return json.decode(response.body);
+      return PostList.fromJson(json.decode(response.body));
     } else {
-      return response.statusCode;
+      throw Exception("Failed to get posts");
     }
   }
 }
